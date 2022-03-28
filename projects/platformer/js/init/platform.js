17dd6d48b77f1032f65cfd3d2e566ccc91020230
gravoisb <brandon.gravois@apsb.org>
2022-03-28 16:46:13 +0000

    add index.html file

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,9 +39,20 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
-        
+        createPlatform(400, 460);           // normal platform
+        createPlatform(300, 200, 0.3)       // small horizontal platform (30% the normal width)
+        createPlatform(500, 500, 0.3, 10)   // tall vertical wall (30% the normal width and 10x the height)
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
     platform.init = init;
 })(window);
+
+function createPlatform(x, y, scaleX, scaleY){
+     /* x: The x coordinate for the platform.
+        y: The y coordinate for the platform.
+        scaleX: OPTIONAL The scale factor on the x-axis, this value will stretch the platform in width.
+        scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the platform in height.
+     */
+
+}
